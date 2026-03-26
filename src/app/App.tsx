a3365b68c038d6d0a384745675003fbe3d474da2
Marcodasilva <marcodasilva@BugsyAir.local>
2026-03-26 13:38:14 +0100

    feat: wire VoiceProvider into app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,19 @@ import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { SettingsProvider } from './contexts/settings-context';
 import { ProgressProvider } from './contexts/progress-context';
+import { VoiceProvider } from './contexts/voice-context';
 import { AchievementWatcher } from './components/achievement-watcher';
 
 export default function App() {
   return (
     <SettingsProvider>
-      <ProgressProvider>
-        <AchievementWatcher>
-          <RouterProvider router={router} />
-        </AchievementWatcher>
-      </ProgressProvider>
+      <VoiceProvider>
+        <ProgressProvider>
+          <AchievementWatcher>
+            <RouterProvider router={router} />
+          </AchievementWatcher>
+        </ProgressProvider>
+      </VoiceProvider>
     </SettingsProvider>
   );
 }
